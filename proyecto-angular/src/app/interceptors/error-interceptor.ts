599ab04f5e200cb8bb5d10a96fdd352f306fa08f
Jorge Cristancho <damjorgecristancho@gmail.com>
2026-02-20 11:36:31 +0100

    segundo

--- proyecto-angular/src/app/interceptors/error-interceptor.ts
+++ proyecto-angular/src/app/interceptors/error-interceptor.ts
@@ -1,5 +1,24 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError(error => {
+
+      if (error.status === 401) {
+        router.navigate(['/error/401']);
+      }
+
+      if (error.status === 403) {
+        router.navigate(['/error/403']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
